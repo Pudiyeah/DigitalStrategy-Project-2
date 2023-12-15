6d8a1919317baaaa6bbc29e46e173c46b76de070
Puding <pudiyeah@gmail.com>
2023-12-15 20:50:03 +0400

    first project

---
+++ OneDrive/Рабочий стол/New folder/first_project.dart
@@ -0,0 +1,16 @@
+void main() {
+  String myName = "Fidan";
+  String mySurname = "Cəfərova";
+  int myAge = 17;
+  double myHeight = 1.58;
+
+  print("salam mən $mySurname $myName.Mənim ${myAge +1} yaşım var");
+  // print(second_name);
+  // print(age);
+  // print(height);
+
+  int a = 27;
+  int b = 18;
+  print(a + b);
+  print(a - b);
+}
